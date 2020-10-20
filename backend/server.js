a60c299308574e0ee9a313f4feb204339492cf02
Ludah Makori <ludahmaxs@gmail.com>
2020-10-20 09:42:27 +0530

    config database connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
 
 const app = express()
+connectDB()
 const PORT = process.env.PORT
 
 // Get route
