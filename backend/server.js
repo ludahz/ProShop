8bc3e45ef76232b1c1560f60ebeceed0c6c21bd3
Ludah Makori <ludahmaxs@gmail.com>
2020-10-20 15:44:54 +0530

    custom error handle

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -13,12 +14,18 @@ connectDB()
 
 const PORT = process.env.PORT
 
+
 // Get route
 app.get('/', (req, res) => {
     res.send('Api is runing...')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+//middleware to handle error in json formart
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
