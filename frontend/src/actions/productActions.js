7725ac83f02ed93461dbc1c5ea05ee5329ec25fc
Ludah Makori <ludahmaxs@gmail.com>
2020-10-21 22:33:24 +0530

    create product details reducer and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCEESS, PRODUCT_LIST_FAIL } from '../constants/productConstants.js'
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCEESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCEESS, PRODUCT_DETAILS_FAIL } from '../constants/productConstants.js'
 
 
 
@@ -7,7 +7,7 @@ export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get('api/products')
+        const { data } = await axios.get('/api/products')
 
         dispatch({ 
             type: PRODUCT_LIST_SUCCEESS,
@@ -17,6 +17,25 @@ export const listProducts = () => async (dispatch) => {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+//Product details
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({ 
+            type: PRODUCT_DETAILS_SUCCEESS,
+            payload: data
+         })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.messgae
         })
     }
 }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,10 +1,11 @@
 //product list reducer, will handle state for product list on the homepage
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCEESS, PRODUCT_LIST_FAIL } from '../constants/productConstants'
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCEESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCEESS, PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
+
 
 export const productListReducer = ( state = { products: [] }, action ) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
-            return { loading:true, product: [] }
+            return { loading:true, products: [] }
         
         case  PRODUCT_LIST_SUCCEESS:
             return { loading:false, products: action.payload }
@@ -16,3 +17,23 @@ export const productListReducer = ( state = { products: [] }, action ) => {
             return state
     }
 }
+
+//product details reducer, will handle state for product list on the homepage
+
+
+export const productDetailsReducer = ( state = { product:  { reviews: [] } }, action ) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading:true, ...state }
+        
+        case  PRODUCT_DETAILS_SUCCEESS:
+            return { loading:false, product: action.payload }
+
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
