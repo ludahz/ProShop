e021cd8fc4880813f9149e46b1de80f1df148428
Ludah Makori <ludahmaxs@gmail.com>
2020-10-22 11:15:34 +0530

    add to cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -13,7 +13,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             image: data.image,
             price: data.price,
             countInStock: data.countInStock,
-            qty: 
+            qty
         }
     })
 
