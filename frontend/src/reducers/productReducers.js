7bcde271d7d74d98c1055db894c0c227121ca254
Ludah Makori <ludahmaxs@gmail.com>
2020-12-05 08:24:13 +0530

    Delete products admin only

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,39 +1,65 @@
 //product list reducer, will handle state for product list on the homepage
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCEESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCEESS, PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+} from '../constants/productConstants'
 
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] }
 
-export const productListReducer = ( state = { products: [] }, action ) => {
-    switch (action.type) {
-        case PRODUCT_LIST_REQUEST:
-            return { loading:true, products: [] }
-        
-        case  PRODUCT_LIST_SUCCEESS:
-            return { loading:false, products: action.payload }
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload }
 
-        case PRODUCT_LIST_FAIL:
-            return { loading: false, error: action.payload }
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
 //product details reducer, will handle state for product list on the homepage
 
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
 
-export const productDetailsReducer = ( state = { product:  { reviews: [] } }, action ) => {
-    switch (action.type) {
-        case PRODUCT_DETAILS_REQUEST:
-            return { loading:true, ...state }
-        
-        case  PRODUCT_DETAILS_SUCCEESS:
-            return { loading:false, product: action.payload }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
 
-        case PRODUCT_DETAILS_FAIL:
-            return { loading: false, error: action.payload }
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true }
+
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true }
+
+    case PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+
+    default:
+      return state
+  }
+}
