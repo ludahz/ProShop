76baf9941b93b038097514a2e8a969cf38430bd9
Ludah Makori <ludahmaxs@gmail.com>
2020-12-16 20:13:45 +0530

    Admin order list

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname)))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
