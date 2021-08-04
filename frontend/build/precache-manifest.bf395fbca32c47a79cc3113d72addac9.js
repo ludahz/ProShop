28bce65d96da5830fc4810c78a9bab2f5e055b8d
Ludah Makori <ludahmaxs@gmail.com>
2021-08-04 22:16:13 +0530

    preparing for deployment

---
+++ frontend/build/precache-manifest.bf395fbca32c47a79cc3113d72addac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772b241790f6ea45299b1c6514b21eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a16699bc97412bc8ae",
+    "url": "/static/css/main.40057908.chunk.css"
+  },
+  {
+    "revision": "e94c6df0cddcfa2f6dd3",
+    "url": "/static/js/2.04b5c48d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.04b5c48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a16699bc97412bc8ae",
+    "url": "/static/js/main.f6819d13.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
